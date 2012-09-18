d51718889468c46c74d9a935b88e4690b15d2169
Rob Maguire <iu65a@hotmail.com>
2012-09-18 12:52:42 -0500

    Added AddItem command

---
+++ SFConsole/data/scripts/console/commands/AddItem.java
@@ -0,0 +1,55 @@
+package data.scripts.console.commands;
+
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.CargoAPI.CargoItemType;
+import data.scripts.console.BaseCommand;
+
+public class AddItem extends BaseCommand
+{
+    @Override
+    protected String getHelp()
+    {
+        return "";
+    }
+
+    @Override
+    protected String getSyntax()
+    {
+        return "additem <itemID> <optionalAmount>";
+    }
+
+    @Override
+    public boolean runCommand(String args)
+    {
+        String[] tmp = args.split(" ");
+
+        if (tmp.length == 1)
+        {
+            return runCommand(args + " 1");
+        }
+
+        if (tmp.length != 2)
+        {
+            showSyntax();
+            return false;
+        }
+
+        float amt;
+
+        try
+        {
+            amt = Float.parseFloat(tmp[1]);
+        }
+        catch (NumberFormatException ex)
+        {
+            showSyntax();
+            return false;
+        }
+
+        Global.getSector().getPlayerFleet().getCargo().addItems(
+                CargoItemType.RESOURCES, tmp[0], amt);
+
+        showMessage("Added " + amt + " of item " + tmp[0] + " to player inventory.");
+        return true;
+    }
+}
