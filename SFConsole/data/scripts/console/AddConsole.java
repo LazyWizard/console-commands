5aaaf28daa00eb555899dc10c03d85ede119ea57
Robert Maguire <LazyWizard@users.noreply.github.com>
2012-10-21 12:02:22 -0500

    Merged mod compatibility fixes from main branch

--- SFConsole/data/scripts/console/AddConsole.java
+++ SFConsole/data/scripts/console/AddConsole.java
@@ -3,16 +3,57 @@ package data.scripts.console;
 import com.fs.starfarer.api.campaign.SectorAPI;
 import com.fs.starfarer.api.campaign.SectorGeneratorPlugin;
 import com.fs.starfarer.api.campaign.StarSystemAPI;
+import java.util.Timer;
+import java.util.TimerTask;
 
 @SuppressWarnings("unchecked")
-public class AddConsole implements SectorGeneratorPlugin
+public final class AddConsole implements SectorGeneratorPlugin
 {
+    private static final String[] SYSTEM_NAMES =
+    {
+        "Corvus", // Vanilla
+        "Caelus", // Project CAELUS
+        "Barnard's Star", // Project Ironclads
+        "Xplo", // Fight for Universe: Sector Xplo
+        "God" // Ascendency
+    };
+
     @Override
     public void generate(SectorAPI sector)
     {
-        StarSystemAPI system = sector.getStarSystem("Corvus");
+        Timer deferredAdd = new Timer(true);
+        deferredAdd.schedule(new DeferredAdd(sector), 100);
+    }
 
-        ConsoleManager consoleManager = new ConsoleManager(system);
-        system.addSpawnPoint(consoleManager);
+    private static class DeferredAdd extends TimerTask
+    {
+        SectorAPI sector;
+
+        public DeferredAdd(SectorAPI sector)
+        {
+            this.sector = sector;
+        }
+
+        @Override
+        public void run()
+        {
+            StarSystemAPI system;
+
+            for (int x = 0; x < SYSTEM_NAMES.length; x++)
+            {
+                system = sector.getStarSystem(SYSTEM_NAMES[x]);
+
+                if (system == null)
+                {
+                    continue;
+                }
+
+                ConsoleManager consoleManager = new ConsoleManager(system);
+                system.addSpawnPoint(consoleManager);
+                return;
+            }
+
+            throw new RuntimeException("Console could not find a starsystem!");
+        }
     }
 }
