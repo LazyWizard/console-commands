55fd0c979c6351f8fd0979ff9b9819e1244f318e
Rob Maguire <iu65a@hotmail.com>
2012-10-21 11:44:48 -0500

    Greatly enhanced mod compatibility

--- SFConsole/data/scripts/console/AddConsole.java
+++ SFConsole/data/scripts/console/AddConsole.java
@@ -3,20 +3,62 @@ package data.scripts.console;
 import com.fs.starfarer.api.campaign.SectorAPI;
 import com.fs.starfarer.api.campaign.SectorGeneratorPlugin;
 import com.fs.starfarer.api.campaign.StarSystemAPI;
+import java.util.Timer;
+import java.util.TimerTask;
 
 @SuppressWarnings("unchecked")
 public final class AddConsole implements SectorGeneratorPlugin
 {
+    private static final String[] SYSTEM_NAMES =
+    {
+        "Corvus",
+        "Caelus",
+        "Barnard's Star",
+        "Xplo",
+        "God"
+    };
+
     @Override
     public void generate(SectorAPI sector)
     {
-        StarSystemAPI system = sector.getStarSystem("Corvus");
-
         ConsoleManager consoleManager = new ConsoleManager();
-        system.addSpawnPoint(consoleManager);
-
         Console.setManager(consoleManager);
-
         ConsoleTests.runTests(consoleManager);
+
+        Timer deferredAdd = new Timer(true);
+        deferredAdd.schedule(new DeferredAdd(sector, consoleManager), 100);
+    }
+
+    private static class DeferredAdd extends TimerTask
+    {
+        SectorAPI sector;
+        ConsoleManager consoleManager;
+
+        public DeferredAdd(SectorAPI sector, ConsoleManager consoleManager)
+        {
+            this.sector = sector;
+            this.consoleManager = consoleManager;
+        }
+
+        @Override
+        public void run()
+        {
+            StarSystemAPI system;
+
+            for (int x = 0; x < SYSTEM_NAMES.length; x++)
+            {
+                system = sector.getStarSystem(SYSTEM_NAMES[x]);
+
+                if (system == null)
+                {
+                    continue;
+                }
+
+                system.addSpawnPoint(consoleManager);
+                return;
+            }
+
+            throw new RuntimeException("Console could not find a starsystem!");
+        }
     }
 }
