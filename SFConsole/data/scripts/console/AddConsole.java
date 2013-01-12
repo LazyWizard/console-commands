cbc0d652d3d4ec05be4c14a9d9f0d8c4edbd6014
Robert Maguire <LazyWizard@users.noreply.github.com>
2013-01-11 22:07:33 -0600

    AddConsole more aggressive, rewrote showMessage() to add in-battle messages

--- SFConsole/data/scripts/console/AddConsole.java
+++ SFConsole/data/scripts/console/AddConsole.java
@@ -1,9 +1,10 @@
 package data.scripts.console;
 
-import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.SectorAPI;
 import com.fs.starfarer.api.campaign.SectorGeneratorPlugin;
 import com.fs.starfarer.api.campaign.StarSystemAPI;
+import java.util.Timer;
+import java.util.TimerTask;
 
 @SuppressWarnings("unchecked")
 public final class AddConsole implements SectorGeneratorPlugin
@@ -11,12 +12,38 @@ public final class AddConsole implements SectorGeneratorPlugin
     @Override
     public void generate(SectorAPI sector)
     {
-        StarSystemAPI system = (StarSystemAPI) Global.getSector().getStarSystems().get(0);
-
-        if (system != null)
+        if (sector.getStarSystems().isEmpty())
         {
+            Timer deferredAdd = new Timer(true);
+            deferredAdd.scheduleAtFixedRate(new DeferredAdd(sector), 1000, 1000);
+        }
+        else
+        {
+            StarSystemAPI system = (StarSystemAPI) sector.getStarSystems().get(0);
             system.addSpawnPoint(new Console());
             Console.showMessage("Console successfully activated for this save.");
+        }
+    }
+
+    private static class DeferredAdd extends TimerTask
+    {
+        private SectorAPI sector;
+
+        public DeferredAdd(SectorAPI sector)
+        {
+            this.sector = sector;
+        }
+
+        @Override
+        public void run()
+        {
+            if (!sector.getStarSystems().isEmpty())
+            {
+                StarSystemAPI system = (StarSystemAPI) sector.getStarSystems().get(0);
+                system.addSpawnPoint(new Console());
+                Console.showMessage("Console successfully activated for this save.");
+                cancel();
+            }
         }
     }
 }
