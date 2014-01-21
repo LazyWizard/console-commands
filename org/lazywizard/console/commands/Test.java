986d32f222f1b09373a6850d2e442ec16b02fca6
LazyWizard <iu65a@hotmail.com>
2014-01-21 16:03:02 -0600

    Implemented Console.showMessage()

--- org/lazywizard/console/commands/Test.java
+++ org/lazywizard/console/commands/Test.java
@@ -2,13 +2,14 @@ package org.lazywizard.console.commands;
 
 import org.lazywizard.console.BaseCommand;
 import org.lazywizard.console.CommandContext;
+import org.lazywizard.console.Console;
 
 public class Test implements BaseCommand
 {
     @Override
     public boolean runCommand(String args, CommandContext context)
     {
-        System.out.println("\n\tRan test command with arguments: " + args + "\n");
+        Console.showMessage("\n\tRan test command with arguments: " + args + "\n");
         return true;
     }
 }
