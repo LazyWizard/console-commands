022d649c447abc112807e0869b1c8f5e74bab739
Robert Maguire <LazyWizard@users.noreply.github.com>
2014-01-21 20:11:01 -0600

    Fixed Echo and Reload

--- org/lazywizard/console/commands/Echo.java
+++ org/lazywizard/console/commands/Echo.java
@@ -1,15 +1,14 @@
 package org.lazywizard.console.commands;
 
 import org.lazywizard.console.BaseCommand;
-import org.lazywizard.console.CommandContext;
 import org.lazywizard.console.Console;
 
 public class Echo implements BaseCommand
 {
     @Override
-    public boolean runCommand(String args, CommandContext context)
+    public CommandResult runCommand(String args, CommandContext context)
     {
         Console.showMessage(args);
-        return true;
+        return CommandResult.SUCCESS;
     }
 }
