eca3ffc2feb6e9a34ae5fa2c88da26322ba62e34
Robert Maguire <LazyWizard@users.noreply.github.com>
2014-01-23 00:00:09 -0600

    Ported AllWeapons, GoTo, Home, SetHome and Status to new system

--- org/lazywizard/console/CommonStrings.java
+++ org/lazywizard/console/CommonStrings.java
@@ -2,9 +2,13 @@ package org.lazywizard.console;
 
 public class CommonStrings
 {
+    // Common error messages
     public static final String ERROR_CAMPAIGN_ONLY = "Error: This command is campaign-only.";
     public static final String ERROR_COMBAT_ONLY = "Error: This command is combat-only.";
     public static final String ERROR_MISSION_ONLY = "Error: This command is mission-only.";
+
+    // Persistent data IDs used by default commands
+    public static final String DATA_HOME_ID = "lw_console_Home";
 
     private CommonStrings()
     {
---
+++ org/lazywizard/console/commands/Home.java
@@ -0,0 +1,58 @@
+package org.lazywizard.console.commands;
+
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.CampaignFleetAPI;
+import com.fs.starfarer.api.campaign.FleetAssignment;
+import com.fs.starfarer.api.campaign.JumpPointAPI.JumpDestination;
+import com.fs.starfarer.api.campaign.LocationAPI;
+import com.fs.starfarer.api.campaign.SectorAPI;
+import com.fs.starfarer.api.campaign.SectorEntityToken;
+import com.fs.starfarer.api.campaign.StarSystemAPI;
+import org.lazywizard.console.BaseCommand;
+import org.lazywizard.console.CommonStrings;
+import org.lazywizard.console.Console;
+import org.lwjgl.util.vector.Vector2f;
+
+public class Home implements BaseCommand
+{
+    @Override
+    public CommandResult runCommand(String args, CommandContext context)
+    {
+        if (context == CommandContext.COMBAT_MISSION)
+        {
+            Console.showMessage(CommonStrings.ERROR_CAMPAIGN_ONLY);
+            return CommandResult.WRONG_CONTEXT;
+        }
+
+        SectorAPI sector = Global.getSector();
+        SectorEntityToken home = (SectorEntityToken) sector.getPersistentData()
+                .get(CommonStrings.DATA_HOME_ID);
+
+        if (home == null)
+        {
+            Console.showMessage("No home found! Use SetHome first!");
+            return CommandResult.ERROR;
+        }
+
+        CampaignFleetAPI playerFleet = sector.getPlayerFleet();
+        LocationAPI loc = home.getContainingLocation();
+        if (loc == playerFleet.getContainingLocation())
+        {
+            Vector2f homeLoc = home.getLocation();
+            playerFleet.setLocation(homeLoc.x, homeLoc.y);
+        }
+        else
+        {
+            sector.doHyperspaceTransition(playerFleet, playerFleet,
+                    new JumpDestination(home, "Teleporting home"));
+        }
+
+        playerFleet.setNoEngaging(2.0f);
+        playerFleet.clearAssignments();
+        playerFleet.addAssignment(FleetAssignment.GO_TO_LOCATION, home, 1);
+        Console.showMessage("Teleported to " + home.getFullName()
+                + " in " + (loc.isHyperspace() ? "hyperspace" : "the "
+                + ((StarSystemAPI) loc).getName()) + " system successfully.");
+        return CommandResult.SUCCESS;
+    }
+}
