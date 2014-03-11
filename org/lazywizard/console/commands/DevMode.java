40d6b08bafe06ed047b1857354b74019b3aebc27
Robert Maguire <LazyWizard@users.noreply.github.com>
2014-03-11 07:20:27 -0500

    Added DevMode toggle command

---
+++ org/lazywizard/console/commands/DevMode.java
@@ -0,0 +1,30 @@
+package org.lazywizard.console.commands;
+
+import com.fs.starfarer.api.Global;
+import org.lazywizard.console.BaseCommand;
+import org.lazywizard.console.Console;
+
+public class DevMode implements BaseCommand
+{
+    @Override
+    public CommandResult runCommand(String args, CommandContext context)
+    {
+        boolean devMode = Global.getSettings().isDevMode();
+
+        if (devMode && "on".equalsIgnoreCase(args))
+        {
+            Console.showMessage("Dev mode is already enabled!");
+            return CommandResult.SUCCESS;
+        }
+        else if (!devMode && "off".equalsIgnoreCase(args))
+        {
+            Console.showMessage("Dev mode is already disabled!");
+            return CommandResult.SUCCESS;
+        }
+
+        devMode = !devMode;
+        Global.getSettings().setDevMode(devMode);
+        Console.showMessage("Dev mode is now " + (devMode ? "enabled." : "disabled."));
+        return CommandResult.SUCCESS;
+    }
+}
