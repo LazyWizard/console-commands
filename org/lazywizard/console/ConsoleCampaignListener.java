200cc957cace4f0ec325221f2450925bcd37a229
LazyWizard <iu65a@hotmail.com>
2014-03-28 17:36:53 -0500

    Merged campaign popup with listener, simplified campaign input check

--- org/lazywizard/console/ConsoleCampaignListener.java
+++ org/lazywizard/console/ConsoleCampaignListener.java
@@ -2,14 +2,18 @@ package org.lazywizard.console;
 
 import com.fs.starfarer.api.EveryFrameScript;
 import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.CustomUIPanelPlugin;
+import com.fs.starfarer.api.campaign.InteractionDialogAPI;
+import com.fs.starfarer.api.campaign.InteractionDialogPlugin;
+import com.fs.starfarer.api.combat.EngagementResultAPI;
+import com.fs.starfarer.api.graphics.PositionAPI;
+import com.fs.starfarer.api.input.InputEventAPI;
+import java.util.List;
 import org.lazywizard.console.BaseCommand.CommandContext;
 import org.lwjgl.input.Keyboard;
 
 public class ConsoleCampaignListener implements EveryFrameScript
 {
-    // Here to work around a LWJGL input bug that will probably never be fixed
-    private transient boolean isPressed = false;
-
     @Override
     public boolean isDone()
     {
@@ -22,48 +26,32 @@ public class ConsoleCampaignListener implements EveryFrameScript
         return true;
     }
 
-    // TODO: replace with something much simpler once the dialog is added
     private boolean checkInput()
     {
         KeyStroke key = Console.getSettings().getConsoleSummonKey();
+        boolean modPressed = true;
 
-        if (!isPressed)
+        if (key.requiresShift() && !(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)
+                || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)))
         {
-            boolean modPressed = true;
-
-            if (key.requiresShift()
-                    && !(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)
-                    || Keyboard.isKeyDown(Keyboard.KEY_RSHIFT)))
-            {
-                modPressed = false;
-            }
-
-            if (key.requiresControl()
-                    && !(Keyboard.isKeyDown(Keyboard.KEY_LCONTROL)
-                    || Keyboard.isKeyDown(Keyboard.KEY_RCONTROL)))
-            {
-                modPressed = false;
-            }
-
-            if (key.requiresAlt()
-                    && !(Keyboard.isKeyDown(Keyboard.KEY_LMENU)
-                    || Keyboard.isKeyDown(Keyboard.KEY_RMENU)))
-            {
-                modPressed = false;
-            }
-
-            if (modPressed && Keyboard.isKeyDown(key.getKey()))
-            {
-                isPressed = true;
-            }
+            modPressed = false;
         }
-        else
+
+        if (key.requiresControl() && !(Keyboard.isKeyDown(Keyboard.KEY_LCONTROL)
+                || Keyboard.isKeyDown(Keyboard.KEY_RCONTROL)))
         {
-            if (!Keyboard.isKeyDown(key.getKey()))
-            {
-                isPressed = false;
-                return true;
-            }
+            modPressed = false;
+        }
+
+        if (key.requiresAlt() && !(Keyboard.isKeyDown(Keyboard.KEY_LMENU)
+                || Keyboard.isKeyDown(Keyboard.KEY_RMENU)))
+        {
+            modPressed = false;
+        }
+
+        if (modPressed && Keyboard.isKeyDown(key.getKey()))
+        {
+            return true;
         }
 
         return false;
@@ -75,17 +63,158 @@ public class ConsoleCampaignListener implements EveryFrameScript
         if (checkInput())
         {
             Global.getSector().getCampaignUI().showInteractionDialog(
-                    new ConsoleCampaignPopup(), null);
-
-            // TODO: write an interaction dialog plugin for this!
-            /*String rawInput = JOptionPane.showInputDialog(null,
-                    "Enter command, or 'help' for a list of valid commands.");
-            Console.parseInput(rawInput, CommandContext.CAMPAIGN_MAP);
-
-            // Temporary!
-            ConsoleCombatListener.resetKeyboard();*/
+                    new CampaignPopup(), null);
         }
 
         Console.advance(CommandContext.CAMPAIGN_MAP);
+    }
+
+    // TODO: Hook this into console output
+    private class CampaignPopup implements InteractionDialogPlugin
+    {
+        private final Object LEAVE = new Object();
+        private InteractionDialogAPI dialog;
+        private KeyListener keyListener;
+        private float timeOpen = 0f;
+
+        @Override
+        public void init(InteractionDialogAPI dialog)
+        {
+            this.dialog = dialog;
+            keyListener = new KeyListener();
+            timeOpen = 0f;
+
+            dialog.getVisualPanel().showCustomPanel(0f, 0f, keyListener);
+            dialog.getTextPanel().addParagraph(CommonStrings.INPUT_QUERY);
+            dialog.setPromptText("Input: ");
+
+            dialog.getOptionPanel().addOption("Cancel", LEAVE);
+            dialog.setOptionOnEscape("Cancel", LEAVE);
+            /*KeyStroke key = Console.getSettings().getConsoleSummonKey();
+             dialog.getOptionPanel().setShortcut(LEAVE,
+             key.getKey(), key.requiresControl(), key.requiresAlt(),
+             key.requiresShift(), true);*/
+        }
+
+        @Override
+        public void optionSelected(String optionText, Object optionData)
+        {
+            if (optionData == LEAVE)
+            {
+                dialog.dismiss();
+            }
+        }
+
+        @Override
+        public void optionMousedOver(String optionText, Object optionData)
+        {
+        }
+
+        @Override
+        public void advance(float amount)
+        {
+            timeOpen += amount;
+            dialog.setPromptText("Input: " + keyListener.currentInput.toString()
+                    + ((((int) timeOpen) & 1) == 0 ? "|" : ""));
+        }
+
+        @Override
+        public void backFromEngagement(EngagementResultAPI battleResult)
+        {
+        }
+
+        @Override
+        public Object getContext()
+        {
+            return null;
+        }
+
+        private class KeyListener implements CustomUIPanelPlugin
+        {
+            StringBuilder currentInput = new StringBuilder();
+
+            public void clearInput()
+            {
+                currentInput.setLength(0);
+            }
+
+            @Override
+            public void positionChanged(PositionAPI position)
+            {
+            }
+
+            @Override
+            public void render(float alphaMult)
+            {
+            }
+
+            @Override
+            public void advance(float amount)
+            {
+            }
+
+            @Override
+            public void processInput(List<InputEventAPI> events)
+            {
+                // TODO: temporary test code; this needs cleanup badly
+                for (InputEventAPI event : events)
+                {
+                    if (event.isConsumed() || !event.isKeyDownEvent()
+                            || event.isModifierKey())
+                    {
+                        continue;
+                    }
+
+                    if (event.getEventValue() == Keyboard.KEY_BACK
+                            && currentInput.length() > 0)
+                    {
+                        // Shift+backspace, delete entire line
+                        if (event.isShiftDown())
+                        {
+                            clearInput();
+                        }
+                        // Control+backspace, delete last word
+                        else if (event.isCtrlDown())
+                        {
+                            int lastSpace = currentInput.lastIndexOf(" ");
+                            if (lastSpace == -1)
+                            {
+                                clearInput();
+                            }
+                            else
+                            {
+                                currentInput.delete(lastSpace, currentInput.length());
+                            }
+                        }
+                        // Regular backspace, delete last character
+                        else
+                        {
+                            currentInput.deleteCharAt(currentInput.length() - 1);
+                        }
+
+                        event.consume();
+                    }
+                    else if (event.getEventValue() == Keyboard.KEY_RETURN)
+                    {
+                        String command = currentInput.toString();
+                        dialog.getTextPanel().addParagraph("Running command \""
+                                + command + "\"", Console.getSettings().getOutputColor());
+                        Console.parseInput(command, CommandContext.CAMPAIGN_MAP);
+                        currentInput.setLength(0);
+                        event.consume();
+                    }
+                    else
+                    {
+                        // Goodbye, international character support...
+                        char character = event.getEventChar();
+                        if (character >= 0x20 && character <= 0x7e)
+                        {
+                            currentInput.append(character);
+                            event.consume();
+                        }
+                    }
+                }
+            }
+        }
     }
 }
