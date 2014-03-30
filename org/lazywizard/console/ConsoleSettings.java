11d8a62661c2d6a257c671d45d363414c9586cc6
Robert Maguire <LazyWizard@users.noreply.github.com>
2014-03-29 23:04:31 -0500

    Added basic Javadoc to ConsoleSettings

--- org/lazywizard/console/ConsoleSettings.java
+++ org/lazywizard/console/ConsoleSettings.java
@@ -8,7 +8,6 @@ import java.awt.Color;
  * @author LazyWizard
  * @since 2.0
  */
-// TODO: Javadoc this
 public class ConsoleSettings
 {
     // The key stroke that summons the console pop-up
@@ -34,31 +33,77 @@ public class ConsoleSettings
         OUTPUT_MAX_LINE_LENGTH = outputMaxLineLength;
     }
 
+    /**
+     * Returns the key (and any modifier keys) that must be pressed to summon
+     * the console.
+     * <p>
+     * @return The {@link KeyStroke} to summon the console.
+     * <p>
+     * @since 2.0
+     */
     public KeyStroke getConsoleSummonKey()
     {
         return CONSOLE_SUMMON_KEY;
     }
 
+    /**
+     * Returns what character sequence the player must separate multiple
+     * commands with.
+     * <p>
+     * @return The {@link String} (usually a single character) that separates
+     *         multiple commands.
+     * <p>
+     * @since 2.0
+     */
     public String getCommandSeparator()
     {
         return COMMAND_SEPARATOR;
     }
 
+    /**
+     * Returns whether each valid individual command should be displayed before
+     * being run.
+     * <p>
+     * @return {@code true} if each command should be displayed to the player
+     *         before executing, {@code false} otherwise.
+     * <p>
+     * @since 2.0
+     */
     public boolean getShouldShowEnteredCommands()
     {
         return SHOW_ENTERED_COMMANDS;
     }
 
+    /**
+     * Returns the {@link Color} of the console's output.
+     * <p>
+     * @return The {@link Color} of the console's output.
+     * <p>
+     * @since 2.0
+     */
     public Color getOutputColor()
     {
         return OUTPUT_COLOR;
     }
 
+    /**
+     * Returns how many characters a line of output can reach before it is
+     * wrapped.
+     * <p>
+     * @return How many characters before the output is line-wrapped
+     * <p>
+     * @since 2.0
+     */
     public int getMaxOutputLineLength()
     {
         return OUTPUT_MAX_LINE_LENGTH;
     }
 
+    /**
+     * Represents the keys that must be pressed to summon the console.
+     * <p>
+     * @since 2.0
+     */
     public class KeyStroke
     {
         private final int key;
@@ -75,21 +120,55 @@ public class ConsoleSettings
             this.requireAlt = requireAlt;
         }
 
+        /**
+         * Returns the key that must be pressed to summon the console.
+         * <p>
+         * The list of valid key codes can be found here:
+         * <a
+         * href=http://www.lwjgl.org/javadoc/constant-values.html#org.lwjgl.input.Keyboard.KEY_0>
+         * http://www.lwjgl.org/javadoc/constant-values.html#org.lwjgl.input.Keyboard.KEY_0</a>
+         * <p>
+         * @return The LWJGL constant of the key that summons the console.
+         * <p>
+         * @since 2.0
+         */
         public int getKey()
         {
             return key;
         }
 
+        /**
+         * Returns whether shift must be held down to summon the console.
+         * <p>
+         * @return Whether you must hold down shift while summoning the console.
+         * <p>
+         * @since 2.0
+         */
         public boolean requiresShift()
         {
             return requireShift;
         }
 
+        /**
+         * Returns whether control must be held down to summon the console.
+         * <p>
+         * @return Whether you must hold down control while summoning the
+         *         console.
+         * <p>
+         * @since 2.0
+         */
         public boolean requiresControl()
         {
             return requireControl;
         }
 
+        /**
+         * Returns whether alt must be held down to summon the console.
+         * <p>
+         * @return Whether you must hold down alt while summoning the console.
+         * <p>
+         * @since 2.0
+         */
         public boolean requiresAlt()
         {
             return requireAlt;
