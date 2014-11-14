523bb2300d0824cd24b5cbfad27de849d1e0688d
LazyWizard <iu65a@hotmail.com>
2014-11-14 14:15:32 -0600

    Added EndCombat command

---
+++ org/lazywizard/console/commands/EndCombat.java
@@ -0,0 +1,45 @@
+package org.lazywizard.console.commands;
+
+import java.util.Arrays;
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.mission.FleetSide;
+import org.lazywizard.console.BaseCommand;
+import org.lazywizard.console.CommonStrings;
+import org.lazywizard.console.Console;
+import org.lazywizard.lazylib.CollectionUtils;
+
+public class EndCombat implements BaseCommand
+{
+    @Override
+    public CommandResult runCommand(String args, CommandContext context)
+    {
+        if (context == CommandContext.CAMPAIGN_MAP
+                || context == CommandContext.COMBAT_SIMULATION)
+        {
+            Console.showMessage(CommonStrings.ERROR_COMBAT_ONLY);
+            return CommandResult.WRONG_CONTEXT;
+        }
+
+        FleetSide victor;
+        if (args.isEmpty())
+        {
+            victor = FleetSide.PLAYER;
+        }
+        else
+        {
+            try
+            {
+                victor = FleetSide.valueOf(args.toUpperCase());
+            }
+            catch (IllegalArgumentException ex)
+            {
+                Console.showMessage("No such side '" + args + "'! Valid sides are "
+                        + CollectionUtils.implode(Arrays.asList(FleetSide.values())) + ".");
+                return CommandResult.ERROR;
+            }
+        }
+
+        Global.getCombatEngine().endCombat(0f, victor);
+        return CommandResult.SUCCESS;
+    }
+}
