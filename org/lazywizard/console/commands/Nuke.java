d3a358bec6eb6a9c66429ff60b08cb69653e5af3
LazyWizard <iu65a@hotmail.com>
2014-11-30 23:53:59 -0600

    Added 'Kill' and 'Suicide' combat commands (request from MesoTronik)

--- org/lazywizard/console/commands/Nuke.java
+++ org/lazywizard/console/commands/Nuke.java
@@ -2,7 +2,6 @@ package org.lazywizard.console.commands;
 
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.combat.CombatEngineAPI;
-import com.fs.starfarer.api.combat.DamageType;
 import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.mission.FleetSide;
 import org.lazywizard.console.BaseCommand;
@@ -10,8 +9,6 @@ import org.lazywizard.console.BaseCommand.CommandContext;
 import org.lazywizard.console.BaseCommand.CommandResult;
 import org.lazywizard.console.CommonStrings;
 import org.lazywizard.console.Console;
-import org.lazywizard.lazylib.CollisionUtils;
-import org.lwjgl.util.vector.Vector2f;
 
 public class Nuke implements BaseCommand
 {
@@ -34,27 +31,7 @@ public class Nuke implements BaseCommand
 
             if (ship.getOwner() == FleetSide.ENEMY.ordinal())
             {
-                Vector2f hitLoc = ship.getLocation();
-
-                // Ensure we hit (needed for certain oddly-shaped mod ships)
-                if (!CollisionUtils.isPointWithinBounds(ship.getLocation(), ship))
-                {
-                    if (!ship.getAllWeapons().isEmpty())
-                    {
-                        //System.out.println("Using alternate hit location for "
-                        //        + ship.getHullSpec().getHullId());
-                        hitLoc = ship.getAllWeapons().get(0).getLocation();
-                    }
-                    else
-                    {
-                        Console.showMessage("Error nuking " + ship.getHullSpec().getHullId());
-                    }
-                }
-
-                // Ensure a kill
-                ship.setHitpoints(1f);
-                engine.applyDamage(ship, hitLoc, 500_000,
-                        DamageType.OTHER, 500_000, true, false, null);
+                Kill.killShip(ship);
             }
         }
 
---
+++ org/lazywizard/console/commands/Suicide.java
@@ -0,0 +1,26 @@
+package org.lazywizard.console.commands;
+
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.combat.ShipAPI;
+import org.lazywizard.console.BaseCommand;
+import org.lazywizard.console.CommonStrings;
+import org.lazywizard.console.Console;
+
+public class Suicide implements BaseCommand
+{
+    @Override
+    public CommandResult runCommand(String args, CommandContext context)
+    {
+        // TODO: Implement suicide in campaign
+        if (context == CommandContext.CAMPAIGN_MAP)
+        {
+            Console.showMessage(CommonStrings.ERROR_COMBAT_ONLY);
+            return CommandResult.WRONG_CONTEXT;
+        }
+
+        ShipAPI player = Global.getCombatEngine().getPlayerShip();
+        Kill.killShip(player);
+        Console.showMessage("Destroyed currently piloted ship.");
+        return CommandResult.SUCCESS;
+    }
+}
