6e305b88469538f92f7f996a87acd550db3c4b1a
Robert Maguire <LazyWizard@users.noreply.github.com>
2014-11-30 23:53:59 -0600

    Added 'Kill' and 'Suicide' combat commands (request from MesoTronik)

---
+++ org/lazywizard/console/commands/Kill.java
@@ -0,0 +1,67 @@
+package org.lazywizard.console.commands;
+
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.combat.CombatEngineAPI;
+import com.fs.starfarer.api.combat.DamageType;
+import com.fs.starfarer.api.combat.ShipAPI;
+import org.lazywizard.console.BaseCommand;
+import org.lazywizard.console.CommonStrings;
+import org.lazywizard.console.Console;
+import org.lazywizard.lazylib.CollisionUtils;
+import org.lwjgl.util.vector.Vector2f;
+
+public class Kill implements BaseCommand
+{
+    public static void killShip(ShipAPI target)
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        // Ensure we hit (needed for certain oddly-shaped mod ships)
+        Vector2f hitLoc = target.getLocation();
+        if (!CollisionUtils.isPointWithinBounds(hitLoc, target))
+        {
+            if (!target.getAllWeapons().isEmpty())
+            {
+                //System.out.println("Using alternate hit location for "
+                //        + ship.getHullSpec().getHullId());
+                hitLoc = target.getAllWeapons().get(0).getLocation();
+            }
+            else
+            {
+                Console.showMessage("Error nuking " + target.getHullSpec().getHullId());
+            }
+        }
+
+        // Ensure a kill
+        target.setHitpoints(1f);
+        Global.getCombatEngine().applyDamage(target, hitLoc, 500_000,
+                DamageType.OTHER, 500_000, true, false, null);
+    }
+
+    @Override
+    public CommandResult runCommand(String args, CommandContext context)
+    {
+        if (context == CommandContext.CAMPAIGN_MAP)
+        {
+            Console.showMessage(CommonStrings.ERROR_COMBAT_ONLY);
+            return CommandResult.WRONG_CONTEXT;
+        }
+
+        CombatEngineAPI engine = Global.getCombatEngine();
+        ShipAPI target = engine.getPlayerShip().getShipTarget();
+
+        if (target == null)
+        {
+            Console.showMessage("No target found!");
+            return CommandResult.ERROR;
+        }
+
+        killShip(target);
+        Console.showMessage("Destroyed " 
+                + target.getVariant().getFullDesignationWithHullName() + ".");
+        return CommandResult.SUCCESS;
+    }
+}
