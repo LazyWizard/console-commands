1b9d57456fed771a3a52688e1969aabb65374bb1
Robert Maguire <LazyWizard@users.noreply.github.com>
2015-02-23 19:17:35 -0600

    Added ToggleAI command (request from MesoTroniK)

---
+++ org/lazywizard/console/commands/ToggleAI.java
@@ -0,0 +1,88 @@
+package org.lazywizard.console.commands;
+
+import java.util.Map;
+import java.util.WeakHashMap;
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.combat.ShipAIPlugin;
+import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.combat.WeaponGroupAPI;
+import org.lazywizard.console.BaseCommand;
+import org.lazywizard.console.CommonStrings;
+import org.lazywizard.console.Console;
+
+public class ToggleAI implements BaseCommand
+{
+    private static final Map<ShipAPI, ShipAIPlugin> ais = new WeakHashMap<>();
+
+    @Override
+    public CommandResult runCommand(String args, CommandContext context)
+    {
+        if (context == CommandContext.CAMPAIGN_MAP)
+        {
+            Console.showMessage(CommonStrings.ERROR_COMBAT_ONLY);
+            return CommandResult.WRONG_CONTEXT;
+        }
+
+        ShipAPI target = Global.getCombatEngine().getPlayerShip().getShipTarget();
+        if (target == null)
+        {
+            Console.showMessage("No target found!");
+            return CommandResult.ERROR;
+        }
+
+        if (ais.containsKey(target))
+        {
+            ShipAIPlugin ai = ais.remove(target);
+            if (ai == null)
+            {
+                target.resetDefaultAI();
+            }
+            else
+            {
+                target.setShipAI(ai);
+            }
+
+            target.getShipAI().forceCircumstanceEvaluation();
+            Console.showMessage("Re-enabled AI of target");
+        }
+        else
+        {
+            System.out.println(target.getShipAI().getClass().getCanonicalName());
+            ais.put(target, target.getShipAI());
+            target.setShipAI(new NullAI());
+            
+            for (WeaponGroupAPI group : target.getWeaponGroupsCopy())
+            {
+                group.toggleOff();
+            }
+
+            Console.showMessage("Disabled AI of target");
+        }
+
+        return CommandResult.SUCCESS;
+    }
+
+    private class NullAI implements ShipAIPlugin
+    {
+        @Override
+        public void setDoNotFireDelay(float amount)
+        {
+        }
+
+        @Override
+        public void forceCircumstanceEvaluation()
+        {
+        }
+
+        @Override
+        public void advance(float amount)
+        {
+        }
+
+        @Override
+        public boolean needsRefit()
+        {
+            return false;
+        }
+    }
+}
