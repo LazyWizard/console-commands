08f30167b908620b9f6d9ca0b62eff7b4babe5e9
Robert Maguire <LazyWizard@users.noreply.github.com>
2015-03-05 21:55:42 -0600

    Started work on getting SpawnFleet functional again

--- org/lazywizard/console/commands/SpawnFleet.java
+++ org/lazywizard/console/commands/SpawnFleet.java
@@ -2,11 +2,14 @@ package org.lazywizard.console.commands;
 
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.CampaignFleetAPI;
+import com.fs.starfarer.api.impl.campaign.fleets.FleetFactory;
 import org.lazywizard.console.BaseCommand;
 import org.lazywizard.console.BaseCommand.CommandContext;
 import org.lazywizard.console.BaseCommand.CommandResult;
 import org.lazywizard.console.CommonStrings;
 import org.lazywizard.console.Console;
+import org.lazywizard.lazylib.MathUtils;
+import org.lwjgl.util.vector.Vector2f;
 
 // TODO: Fix this to work with new .65a fleet spawning behavior
 public class SpawnFleet implements BaseCommand
@@ -37,9 +40,13 @@ public class SpawnFleet implements BaseCommand
 
         try
         {
-            CampaignFleetAPI toSpawn = Global.getSector().createFleet(faction, fleet);
+
+            final CampaignFleetAPI toSpawn
+                    //= Global.getSector().createFleet(faction, fleet);
+                    = FleetFactory.createGenericFleet(faction, fleet, 1f, 100);
+            final Vector2f offset = MathUtils.getRandomPointOnCircumference(null, 150f);
             Global.getSector().getCurrentLocation().spawnFleet(
-                    Global.getSector().getPlayerFleet(), 50, 50, toSpawn);
+                    Global.getSector().getPlayerFleet(), offset.x, offset.y, toSpawn);
         }
         catch (Exception ex)
         {
