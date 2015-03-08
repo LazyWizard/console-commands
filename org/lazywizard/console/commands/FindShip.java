d256930f2f4c4b62bfc3c6b0d89fce52f4be6160
Robert Maguire <LazyWizard@users.noreply.github.com>
2015-03-08 04:31:57 -0500

    FindShip now displays local prices and legality for ships and wings

--- org/lazywizard/console/commands/FindShip.java
+++ org/lazywizard/console/commands/FindShip.java
@@ -3,6 +3,7 @@ package org.lazywizard.console.commands;
 import java.util.HashMap;
 import java.util.Map;
 import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.SubmarketPlugin.TransferAction;
 import com.fs.starfarer.api.campaign.econ.MarketAPI;
 import com.fs.starfarer.api.campaign.econ.SubmarketAPI;
 import com.fs.starfarer.api.fleet.FleetMemberAPI;
@@ -35,23 +36,41 @@ public class FindShip implements BaseCommand
             args = args.substring(0, args.lastIndexOf("_wing"));
         }
 
-        Map<SubmarketAPI, Integer> found = new HashMap<>();
+        float shipPriceMod = Global.getSettings().getFloat("shipBuyPriceMult");
+
+        Map<SubmarketAPI, PriceData> found = new HashMap<>();
         for (MarketAPI market : Global.getSector().getEconomy().getMarketsCopy())
         {
             for (SubmarketAPI submarket : market.getSubmarketsCopy())
             {
                 int total = 0;
+                boolean isFirstFound = true;
+                float price = 0f;
+                boolean isIllegal = false;
                 for (FleetMemberAPI member : submarket.getCargo().getMothballedShips().getMembersListCopy())
                 {
                     if (member.getHullId().equalsIgnoreCase(args))
                     {
                         total++;
+
+                        if (isFirstFound)
+                        {
+                            isFirstFound = false;
+
+                            if (!submarket.getPlugin().isFreeTransfer())
+                            {
+                                price = member.getBaseBuyValue();
+                                price += (price * submarket.getTariff());
+                                isIllegal = submarket.getPlugin().isIllegalOnSubmarket(
+                                        member, TransferAction.PLAYER_BUY);
+                            }
+                        }
                     }
                 }
 
                 if (total > 0)
                 {
-                    found.put(submarket, total);
+                    found.put(submarket, new PriceData(price, total, isIllegal));
                 }
             }
         }
@@ -64,16 +83,48 @@ public class FindShip implements BaseCommand
 
         Console.showMessage("Found " + found.size() + " markets with "
                 + Character.toUpperCase(args.charAt(0)) + args.substring(1) + "s for sale:");
-        for (Map.Entry<SubmarketAPI, Integer> entry : found.entrySet())
+        for (Map.Entry<SubmarketAPI, PriceData> entry : found.entrySet())
         {
             SubmarketAPI submarket = entry.getKey();
-            Console.showMessage(" - " + entry.getValue() + " for sale in "
+            PriceData data = entry.getValue();
+            Console.showMessage(" - " + data.getAvailable() + " available for "
+                    + Math.round(data.getPrice()) + " credits each at "
                     + submarket.getMarket().getName() + "'s "
                     + submarket.getNameOneLine() + " submarket ("
                     + submarket.getFaction().getDisplayName() + ", "
                     + submarket.getMarket().getPrimaryEntity()
-                    .getContainingLocation().getName() + ")");
+                    .getContainingLocation().getName()
+                    + (data.isIllegal() ? ", restricted)" : ")"));
         }
         return CommandResult.SUCCESS;
+    }
+
+    private class PriceData
+    {
+        private final float pricePer;
+        private final int totalAvailable;
+        private final boolean isIllegal;
+
+        private PriceData(float pricePer, int totalAvailable, boolean isIllegal)
+        {
+            this.pricePer = pricePer;
+            this.totalAvailable = totalAvailable;
+            this.isIllegal = isIllegal;
+        }
+
+        private float getPrice()
+        {
+            return pricePer;
+        }
+
+        private int getAvailable()
+        {
+            return totalAvailable;
+        }
+
+        private boolean isIllegal()
+        {
+            return isIllegal;
+        }
     }
 }
