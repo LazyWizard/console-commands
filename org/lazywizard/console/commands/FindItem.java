9fdc3638552294781e22ee3756502729612cb158
Robert Maguire <LazyWizard@users.noreply.github.com>
2015-03-07 19:00:24 -0600

    FindItem now displays local prices and legality for commidities and weapons

--- org/lazywizard/console/commands/FindItem.java
+++ org/lazywizard/console/commands/FindItem.java
@@ -3,7 +3,10 @@ package org.lazywizard.console.commands;
 import java.util.HashMap;
 import java.util.Map;
 import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.CargoAPI;
 import com.fs.starfarer.api.campaign.CargoAPI.CargoItemType;
+import com.fs.starfarer.api.campaign.CargoStackAPI;
+import com.fs.starfarer.api.campaign.SubmarketPlugin;
 import com.fs.starfarer.api.campaign.econ.MarketAPI;
 import com.fs.starfarer.api.campaign.econ.SubmarketAPI;
 import org.lazywizard.console.BaseCommand;
@@ -13,6 +16,7 @@ import org.lazywizard.console.CommandUtils;
 import org.lazywizard.console.CommonStrings;
 import org.lazywizard.console.Console;
 
+// TODO: Needs some cleanup, a lot of weapon/item code can be merged
 public class FindItem implements BaseCommand
 {
     @Override
@@ -44,7 +48,17 @@ public class FindItem implements BaseCommand
             }
         }
 
-        Map<SubmarketAPI, Integer> found = new HashMap<>();
+        // Weapon analysis has to be done through a cargo stack
+        CargoStackAPI stack = null;
+        float weaponPriceMod = Global.getSettings().getFloat("nonEconItemBuyPriceMult");
+        if (isWeapon)
+        {
+            CargoAPI tmp = Global.getFactory().createCargo(false);
+            tmp.addWeapons(id, 1);
+            stack = tmp.getStacksCopy().get(0);
+        }
+
+        Map<SubmarketAPI, PriceData> found = new HashMap<>();
         for (MarketAPI market : Global.getSector().getEconomy().getMarketsCopy())
         {
             for (SubmarketAPI submarket : market.getSubmarketsCopy())
@@ -54,7 +68,33 @@ public class FindItem implements BaseCommand
                         id);
                 if (total > 0)
                 {
-                    found.put(submarket, total);
+                    float price;
+                    boolean isIllegal;
+
+                    if (submarket.getPlugin().isFreeTransfer())
+                    {
+                        price = 0;
+                        isIllegal = false;
+                    }
+                    else
+                    {
+                        if (isWeapon)
+                        {
+                            price = stack.getBaseValuePerUnit() * weaponPriceMod;
+                            price += (price * submarket.getTariff());
+                            isIllegal = submarket.getPlugin().isIllegalOnSubmarket(
+                                    stack, SubmarketPlugin.TransferAction.PLAYER_BUY);
+                        }
+                        else
+                        {
+                            price = market.getSupplyPrice(id, 1f, true);
+                            price += (price * submarket.getTariff());
+                            isIllegal = submarket.getPlugin().isIllegalOnSubmarket(
+                                    id, SubmarketPlugin.TransferAction.PLAYER_BUY);
+                        }
+                    }
+
+                    found.put(submarket, new PriceData(price, total, isIllegal));
                 }
             }
         }
@@ -67,19 +107,49 @@ public class FindItem implements BaseCommand
         }
 
         Console.showMessage("Found " + found.size() + " markets with "
-                + (isWeapon ? "weapon '" : " commodity '")
-                + Character.toUpperCase(id.charAt(0)) + id.substring(1)
-                + "' for sale:");
-        for (Map.Entry<SubmarketAPI, Integer> entry : found.entrySet())
+                + (isWeapon ? "weapon '" : " commodity '") + id + "' for sale:");
+        for (Map.Entry<SubmarketAPI, PriceData> entry : found.entrySet())
         {
             SubmarketAPI submarket = entry.getKey();
-            Console.showMessage(" - " + entry.getValue() + " for sale in "
+            PriceData data = entry.getValue();
+            Console.showMessage(" - " + data.getAvailable() + " available for "
+                    + Math.round(data.getPrice()) + " credits each at "
                     + submarket.getMarket().getName() + "'s "
                     + submarket.getNameOneLine() + " submarket ("
                     + submarket.getFaction().getDisplayName() + ", "
                     + submarket.getMarket().getPrimaryEntity()
-                    .getContainingLocation().getName() + ")");
+                    .getContainingLocation().getName()
+                    + (data.isIllegal() ? ", restricted)" : ")"));
         }
         return CommandResult.SUCCESS;
+    }
+
+    private class PriceData
+    {
+        private final float pricePer;
+        private final int totalAvailable;
+        private final boolean isIllegal;
+
+        private PriceData(float pricePer, int totalAvailable, boolean isIllegal)
+        {
+            this.pricePer = pricePer;
+            this.totalAvailable = totalAvailable;
+            this.isIllegal = isIllegal;
+        }
+
+        private float getPrice()
+        {
+            return pricePer;
+        }
+
+        private int getAvailable()
+        {
+            return totalAvailable;
+        }
+
+        private boolean isIllegal()
+        {
+            return isIllegal;
+        }
     }
 }
