7eb73d16902e4ba0e13fd14e5bdd5715f113c716
Robert Maguire <Robert Maguire@localhost>
2015-11-27 17:48:17 -0600

    Adjust/SetRelationship: assume second faction is player if none was entered

--- org/lazywizard/console/commands/SetRelationship.java
+++ org/lazywizard/console/commands/SetRelationship.java
@@ -24,6 +24,11 @@ public class SetRelationship implements BaseCommand
 
         String[] tmp = args.split(" ");
 
+        if (tmp.length == 2)
+        {
+            return runCommand(tmp[0] + " player " + tmp[1], context);
+        }
+
         if (tmp.length != 3)
         {
             return CommandResult.BAD_SYNTAX;
