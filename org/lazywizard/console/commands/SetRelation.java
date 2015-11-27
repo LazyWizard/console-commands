d4bb9ae9f25eef54199977461753dac57cc1a27b
Robert Maguire <LazyWizard@users.noreply.github.com>
2015-11-27 17:59:14 -0600

    Shortened Adjust/SetRelationship to Adjust/SetRelation

--- org/lazywizard/console/commands/SetRelation.java
+++ org/lazywizard/console/commands/SetRelation.java
@@ -6,7 +6,7 @@ import org.lazywizard.console.CommandUtils;
 import org.lazywizard.console.CommonStrings;
 import org.lazywizard.console.Console;
 
-public class SetRelationship implements BaseCommand
+public class SetRelation implements BaseCommand
 {
     @Override
     public CommandResult runCommand(String args, CommandContext context)
