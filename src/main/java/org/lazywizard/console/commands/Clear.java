29624810124c0b5eed27ac56dbaf1264169eec56
Robert Maguire <LazyWizard@users.noreply.github.com>
2018-11-04 00:26:28 -0500

    Properly implemented Clear command (now works when used with separators)

--- src/main/java/org/lazywizard/console/commands/Clear.java
+++ src/main/java/org/lazywizard/console/commands/Clear.java
@@ -1,39 +1,15 @@
 package org.lazywizard.console.commands;
 
-import com.fs.starfarer.api.Global;
-import com.fs.starfarer.api.campaign.InteractionDialogAPI;
 import org.lazywizard.console.BaseCommand;
-import org.lazywizard.console.CommonStrings;
 import org.lazywizard.console.Console;
 
-// TODO: Update for new overlay
+// Only included for the helpfile and will never run - Clear support is hardcoded into the overlay
 public class Clear implements BaseCommand
 {
     @Override
     public CommandResult runCommand(String args, CommandContext context)
     {
-        if (!context.isInCampaign())
-        {
-            Console.showMessage(CommonStrings.ERROR_CAMPAIGN_ONLY);
-            return CommandResult.WRONG_CONTEXT;
-        }
-
-        final InteractionDialogAPI dialog = Global.getSector().getCampaignUI().getCurrentInteractionDialog();
-        if (dialog == null)
-        {
-            Console.showMessage("No dialog found!");
-            return CommandResult.ERROR;
-        }
-
-        final float oldXOffset = dialog.getXOffset(),
-                oldYOffset = dialog.getYOffset(),
-                oldTextWidth = dialog.getTextWidth(),
-                oldTextHeight = dialog.getTextHeight();
-        dialog.getTextPanel().clear();
-        dialog.setTextWidth(oldTextWidth);
-        dialog.setTextHeight(oldTextHeight);
-        dialog.setXOffset(oldXOffset);
-        dialog.setYOffset(oldYOffset);
+        Console.showMessage("This is only a dummy implementation. How did you do this?");
         return CommandResult.SUCCESS;
     }
 }
