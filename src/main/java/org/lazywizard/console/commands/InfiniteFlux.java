2d0c97e1dc3b77fa46282f8cd0b23d1fa29d9e81
Robert Maguire <Robert Maguire@localhost>
2018-11-03 22:47:57 -0500

    Ported InfiniteFlux to use CombatCheatManager

--- src/main/java/org/lazywizard/console/commands/InfiniteFlux.java
+++ src/main/java/org/lazywizard/console/commands/InfiniteFlux.java
@@ -1,23 +1,19 @@
 package org.lazywizard.console.commands;
 
-import java.lang.ref.WeakReference;
-import java.util.List;
-import com.fs.starfarer.api.Global;
-import com.fs.starfarer.api.combat.BaseEveryFrameCombatPlugin;
-import com.fs.starfarer.api.combat.CombatEngineAPI;
 import com.fs.starfarer.api.combat.FluxTrackerAPI;
 import com.fs.starfarer.api.combat.ShipAPI;
-import com.fs.starfarer.api.input.InputEventAPI;
-import com.fs.starfarer.api.mission.FleetSide;
+import org.jetbrains.annotations.NotNull;
 import org.lazywizard.console.BaseCommand;
-import org.lazywizard.console.BaseCommand.CommandContext;
-import org.lazywizard.console.BaseCommand.CommandResult;
 import org.lazywizard.console.CommonStrings;
 import org.lazywizard.console.Console;
+import org.lazywizard.console.cheatmanager.CheatPlugin;
+import org.lazywizard.console.cheatmanager.CheatTarget;
+import org.lazywizard.console.cheatmanager.CombatCheatManager;
+import org.lazywizard.lazylib.CollectionUtils;
 
 public class InfiniteFlux implements BaseCommand
 {
-    private static WeakReference<InfiniteFluxPlugin> plugin;
+    private static final String CHEAT_ID = "lw_console_infiniteflux";
 
     @Override
     public CommandResult runCommand(String args, CommandContext context)
@@ -28,68 +24,59 @@ public class InfiniteFlux implements BaseCommand
             return CommandResult.WRONG_CONTEXT;
         }
 
-        InfiniteFluxPlugin tmp;
-        if (plugin == null || plugin.get() == null
-                || plugin.get().engine != Global.getCombatEngine())
+        // If no argument is entered:
+        // - If cheat is not enabled, enable it for the player only
+        // - If cheat is already enabled, disable it
+        CheatTarget appliesTo = null;
+        if (args.isEmpty())
         {
-            tmp = new InfiniteFluxPlugin();
-            plugin = new WeakReference<>(tmp);
-            Global.getCombatEngine().addPlugin(tmp);
-            Console.showMessage("Infinite flux enabled.");
-        }
-        else
-        {
-            tmp = plugin.get();
-            plugin.clear();
-            tmp.active = false;
-            Console.showMessage("Infinite flux disabled.");
+            if (CombatCheatManager.isEnabled(CHEAT_ID))
+            {
+                CombatCheatManager.disableCheat(CHEAT_ID);
+                Console.showMessage("Infinite flux disabled.");
+                return CommandResult.SUCCESS;
+            }
+            else
+            {
+                appliesTo = CheatTarget.PLAYER;
+            }
         }
 
+        // If argument is entered, try to parse it as a valid cheat target
+        if (appliesTo == null)
+        {
+            appliesTo = CombatCheatManager.parseTargets(args);
+            if (appliesTo == null)
+            {
+                Console.showMessage("Bad target! Valid targets: " + CollectionUtils.implode(CheatTarget.class) + ".");
+                return CommandResult.ERROR;
+            }
+        }
+
+        CombatCheatManager.enableCheat(CHEAT_ID, "Infinite Flux (" + appliesTo.name() + ")",
+                new InfiniteFluxPlugin(), appliesTo);
+        Console.showMessage("Infinite flux enabled for " + appliesTo.name() + ".");
         return CommandResult.SUCCESS;
     }
 
-    private static class InfiniteFluxPlugin extends BaseEveryFrameCombatPlugin
+    private static class InfiniteFluxPlugin extends CheatPlugin
     {
-        private boolean active = true;
-        private CombatEngineAPI engine;
-
         @Override
-        public void advance(float amount, List<InputEventAPI> events)
+        public void advance(@NotNull ShipAPI ship, float amount)
         {
-            if (!active)
-            {
-                engine.removePlugin(this);
-                return;
-            }
-
-            if (engine.isPaused())
+            if (ship.isHulk() || ship.isShuttlePod())
             {
                 return;
             }
 
-            for (ShipAPI ship : engine.getShips())
+            final FluxTrackerAPI flux = ship.getFluxTracker();
+            flux.setCurrFlux(0f);
+            flux.setHardFlux(0f);
+
+            if (flux.isOverloaded())
             {
-                if (ship.isHulk() || ship.isShuttlePod()
-                        || !(ship.getOwner() == FleetSide.PLAYER.ordinal()))
-                {
-                    continue;
-                }
-
-                FluxTrackerAPI flux = ship.getFluxTracker();
-                flux.setCurrFlux(0f);
-                flux.setHardFlux(0f);
-
-                if (flux.isOverloaded())
-                {
-                    flux.stopOverload();
-                }
+                flux.stopOverload();
             }
-        }
-
-        @Override
-        public void init(CombatEngineAPI engine)
-        {
-            this.engine = engine;
         }
     }
 }
