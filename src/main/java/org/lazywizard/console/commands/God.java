04ea73b4b8d465d1582b9fce021a413451d6ca52
Robert Maguire <LazyWizard@users.noreply.github.com>
2018-11-17 22:31:39 -0600

    Ported God to use CombatCheatManager

--- src/main/java/org/lazywizard/console/commands/God.java
+++ src/main/java/org/lazywizard/console/commands/God.java
@@ -1,23 +1,19 @@
 package org.lazywizard.console.commands;
 
-import java.lang.ref.WeakReference;
-import java.util.List;
-import com.fs.starfarer.api.Global;
-import com.fs.starfarer.api.combat.BaseEveryFrameCombatPlugin;
-import com.fs.starfarer.api.combat.CombatEngineAPI;
 import com.fs.starfarer.api.combat.ShipAPI;
-import com.fs.starfarer.api.input.InputEventAPI;
-import com.fs.starfarer.api.mission.FleetSide;
-import com.fs.starfarer.api.util.IntervalUtil;
+import org.jetbrains.annotations.NotNull;
 import org.lazywizard.console.BaseCommand;
-import org.lazywizard.console.BaseCommand.CommandContext;
-import org.lazywizard.console.BaseCommand.CommandResult;
 import org.lazywizard.console.CommonStrings;
 import org.lazywizard.console.Console;
+import org.lazywizard.console.cheatmanager.CheatPlugin;
+import org.lazywizard.console.cheatmanager.CheatTarget;
+import org.lazywizard.console.cheatmanager.CombatCheatManager;
+import org.lazywizard.lazylib.CollectionUtils;
 
+// FIXME: Ships will not shoot at invincible targets in 0.9a
 public class God implements BaseCommand
 {
-    private static WeakReference<GodPlugin> plugin;
+    private static final String CHEAT_ID = "lw_console_god";
 
     @Override
     public CommandResult runCommand(String args, CommandContext context)
@@ -28,87 +24,57 @@ public class God implements BaseCommand
             return CommandResult.WRONG_CONTEXT;
         }
 
-        GodPlugin tmp;
-        if (plugin == null || plugin.get() == null
-                || plugin.get().engine != Global.getCombatEngine())
+        // If no argument is entered:
+        // - If cheat is not enabled, enable it for the player only
+        // - If cheat is already enabled, disable it
+        CheatTarget appliesTo = null;
+        if (args.isEmpty())
         {
-            tmp = new GodPlugin();
-            plugin = new WeakReference<>(tmp);
-            Global.getCombatEngine().addPlugin(tmp);
-            Console.showMessage("God mode enabled.");
-        }
-        else
-        {
-            tmp = plugin.get();
-            plugin.clear();
-            tmp.active = false;
-            Console.showMessage("God mode disabled.");
+            if (CombatCheatManager.isEnabled(CHEAT_ID))
+            {
+                CombatCheatManager.disableCheat(CHEAT_ID);
+                Console.showMessage("God mode disabled.");
+                return CommandResult.SUCCESS;
+            }
+            else
+            {
+                appliesTo = CheatTarget.PLAYER;
+            }
         }
 
+        // If argument is entered, try to parse it as a valid cheat target
+        if (appliesTo == null)
+        {
+            appliesTo = CombatCheatManager.parseTargets(args);
+            if (appliesTo == null)
+            {
+                Console.showMessage("Bad target! Valid targets: " + CollectionUtils.implode(CheatTarget.class) + ".");
+                return CommandResult.ERROR;
+            }
+        }
+
+        CombatCheatManager.enableCheat(CHEAT_ID, "God Mode (" + appliesTo.name() + ")",
+                new GodPlugin(), appliesTo);
+        Console.showMessage("God mode enabled for " + appliesTo.name() + ".");
         return CommandResult.SUCCESS;
     }
 
-    private static class GodPlugin extends BaseEveryFrameCombatPlugin
+    private static class GodPlugin extends CheatPlugin
     {
-        private static final String BONUS_ID = "console_god";
-        private final IntervalUtil nextCheck = new IntervalUtil(0.5f, 0.5f);
-        private boolean active = true, firstRun = true;
-        private CombatEngineAPI engine;
-
         @Override
-        public void advance(float amount, List<InputEventAPI> events)
+        public void advance(@NotNull ShipAPI ship, float amount)
         {
-            if (!active)
-            {
-                for (ShipAPI ship : engine.getShips())
-                {
-                    if (ship.isHulk() || ship.isShuttlePod()
-                            || !(ship.getOwner() == FleetSide.PLAYER.ordinal()))
-                    {
-                        continue;
-                    }
-
-                    ship.getMutableStats().getHullDamageTakenMult().unmodify(BONUS_ID);
-                    ship.getMutableStats().getEmpDamageTakenMult().unmodify(BONUS_ID);
-                    ship.getMutableStats().getArmorDamageTakenMult().unmodify(BONUS_ID);
-                }
-
-                engine.removePlugin(this);
-                return;
-            }
-
-            if (engine.isPaused())
-            {
-                return;
-            }
-
-            nextCheck.advance(amount);
-            if (firstRun || nextCheck.intervalElapsed())
-            {
-                firstRun = false;
-
-                for (ShipAPI ship : engine.getShips())
-                {
-                    if (ship.isHulk() || ship.isShuttlePod()
-                            || !(ship.getOwner() == FleetSide.PLAYER.ordinal()))
-                    {
-                        continue;
-                    }
-
-                    ship.getMutableStats().getHullDamageTakenMult()
-                            .modifyMult(BONUS_ID, 0f);
-                    ship.getMutableStats().getEmpDamageTakenMult()
-                            .modifyMult(BONUS_ID, 0f);
-                    ship.getMutableStats().getArmorDamageTakenMult()
-                            .modifyMult(BONUS_ID, 0.00001f);
-                }
-            }
+            ship.getMutableStats().getHullDamageTakenMult().modifyMult(CHEAT_ID, 0f);
+            ship.getMutableStats().getEmpDamageTakenMult().modifyMult(CHEAT_ID, 0f);
+            ship.getMutableStats().getArmorDamageTakenMult().modifyMult(CHEAT_ID, 0.00001f);
         }
 
         @Override
-        public void init(CombatEngineAPI engine)
+        public void unapply(@NotNull ShipAPI ship)
         {
-            this.engine = engine;
+            ship.getMutableStats().getHullDamageTakenMult().unmodify(CHEAT_ID);
+            ship.getMutableStats().getEmpDamageTakenMult().unmodify(CHEAT_ID);
+            ship.getMutableStats().getArmorDamageTakenMult().unmodify(CHEAT_ID);
         }
     }
 }
