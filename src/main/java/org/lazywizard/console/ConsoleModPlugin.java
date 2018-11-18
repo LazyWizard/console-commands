95eb45c03dde1e022922d54d5f07af06b1e73511
Robert Maguire <Robert Maguire@localhost>
2018-11-18 03:56:42 -0600

    DebugDialogs: Don't persist between sessions

--- src/main/java/org/lazywizard/console/ConsoleModPlugin.java
+++ src/main/java/org/lazywizard/console/ConsoleModPlugin.java
@@ -2,11 +2,15 @@ package org.lazywizard.console;
 
 import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.rules.MemoryAPI;
 import org.apache.log4j.Level;
+import org.lazywizard.console.commands.DebugDialogs;
 import org.lazywizard.console.commands.ReloadConsole;
 
 public class ConsoleModPlugin extends BaseModPlugin
 {
+    private boolean debugDialogs = false;
+
     @Override
     public void onApplicationLoad() throws Exception
     {
@@ -21,5 +25,19 @@ public class ConsoleModPlugin extends BaseModPlugin
     public void onGameLoad(boolean newGame)
     {
         Global.getSector().getListenerManager().addListener(new ConsoleCampaignListener(), true);
+    }
+
+    @Override
+    public void beforeGameSave()
+    {
+        final MemoryAPI memory = Global.getSector().getMemory();
+        debugDialogs = memory.getBoolean(DebugDialogs.MEMORY_KEY);
+        if (debugDialogs) memory.unset(DebugDialogs.MEMORY_KEY);
+    }
+
+    @Override
+    public void afterGameSave()
+    {
+        if (debugDialogs) Global.getSector().getMemory().set(DebugDialogs.MEMORY_KEY, true);
     }
 }
