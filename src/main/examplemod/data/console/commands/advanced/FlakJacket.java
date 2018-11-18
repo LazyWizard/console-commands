6e078407598c3450206ef94171f6449a357af289
Robert Maguire <LazyWizard@users.noreply.github.com>
2018-11-18 01:56:31 -0600

    Updated Example Commands mini-mod for 0.9a/new CombatCheatManager system

--- src/main/examplemod/data/console/commands/advanced/FlakJacket.java
+++ src/main/examplemod/data/console/commands/advanced/FlakJacket.java
@@ -2,6 +2,7 @@ package data.console.commands.advanced;
 
 import java.lang.ref.WeakReference;
 import java.util.List;
+
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.combat.BaseEveryFrameCombatPlugin;
 import com.fs.starfarer.api.combat.CombatEngineAPI;
@@ -11,11 +12,13 @@ import com.fs.starfarer.api.util.IntervalUtil;
 import org.lazywizard.console.BaseCommand;
 import org.lazywizard.console.CommonStrings;
 import org.lazywizard.console.Console;
+import org.lazywizard.console.cheatmanager.CheatPlugin;
+import org.lazywizard.console.cheatmanager.CombatCheatManager;
 import org.lazywizard.lazylib.MathUtils;
 
 public class FlakJacket implements BaseCommand
 {
-    private static WeakReference plugin = new WeakReference(null);
+    private static final String CHEAT_ID = "lw_console_flakjacket";
 
     @Override
     public CommandResult runCommand(String args, CommandContext context)
@@ -26,48 +29,34 @@ public class FlakJacket implements BaseCommand
             return CommandResult.WRONG_CONTEXT;
         }
 
-        FlakJacketPlugin tmp = (FlakJacketPlugin) plugin.get();
-        if (tmp == null || tmp.engine != Global.getCombatEngine())
+        if (CombatCheatManager.isEnabled(CHEAT_ID))
         {
-            tmp = new FlakJacketPlugin();
-            plugin = new WeakReference(tmp);
-            Global.getCombatEngine().addPlugin(tmp);
-            Console.showMessage("Flak jacket enabled.");
+            CombatCheatManager.disableCheat(CHEAT_ID);
+            Console.showMessage("Flak jacket disabled.");
+            return CommandResult.SUCCESS;
         }
         else
         {
-            plugin.clear();
-            tmp.active = false;
-            Console.showMessage("Flak jacket disabled.");
+            CombatCheatManager.enableCheat(CHEAT_ID, "Flak Jacket", new FlakJacketPlugin(), null);
+            Console.showMessage("Flak jacket enabled.");
+            return CommandResult.SUCCESS;
         }
-
-        return CommandResult.SUCCESS;
     }
 
-    private static class FlakJacketPlugin extends BaseEveryFrameCombatPlugin
+    private static class FlakJacketPlugin extends CheatPlugin
     {
         private final IntervalUtil nextCheck = new IntervalUtil(0.25f, 0.25f);
         private boolean active = true;
         private CombatEngineAPI engine;
 
         @Override
-        public void advance(float amount, List<InputEventAPI> events)
+        public void advance(float amount, List events)
         {
-            if (!active)
-            {
-                engine.removePlugin(this);
-                return;
-            }
-
-            if (engine.isPaused())
-            {
-                return;
-            }
-
             nextCheck.advance(amount);
             if (nextCheck.intervalElapsed())
             {
-                ShipAPI player = engine.getPlayerShip();
+                final CombatEngineAPI engine = Global.getCombatEngine();
+                final ShipAPI player = engine.getPlayerShip();
                 for (int x = 0; x < 360; x += 10)
                 {
                     engine.spawnProjectile(player, null, "flak",
@@ -79,9 +68,9 @@ public class FlakJacket implements BaseCommand
         }
 
         @Override
-        public void init(CombatEngineAPI engine)
+        public boolean runWhilePaused()
         {
-            this.engine = engine;
+            return false;
         }
     }
 }
